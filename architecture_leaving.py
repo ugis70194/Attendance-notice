f8bf2198a9dc0dc926864f5d98125dde06af3114
ugis70194 <ugis70194@yahoo.co.jp>
2020-05-18 15:28:14 +0900

    fix

--- architecture_leaving.py
+++
@@ -1,6 +0,0 @@
-from base import *
-import json
-import os 
-
-data = json.dumps({"content" : "計算機アーキテクチャ退席確認: " + os.environ["architecture_leaving"]})
-response = session.post(webhook, headers=header, data=data)
